5f2449a39a1bbde245da93daecdcfd0b375cdd04
scarf <greenscarf005@gmail.com>
2022-03-17 12:11:07 +0900

    release@v1.0.4

--- src/hgen/_version.py
+++ src/hgen/_version.py
@@ -1,12 +1,4 @@
 from importlib.metadata import version
 from pathlib import Path
 
-# def load_version():
-#     path = Path("setup.cfg")
-#     if not path.exists():
-#         path = Path("../setup.cfg")
-#     file = ConfigParser()
-#     file.read(path)
-#     return file["metadata"]["version"]
-
 __version__ = version('hgen')
